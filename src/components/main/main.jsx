91c7d99e0e13195dedb6a972dba2a8be53245dc3
Пользователь <otabeksediyorov298@gmail.com>
2024-08-07 09:12:10 +0500

    s

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -4,17 +4,21 @@ import rasm from "../../assets/rasm.jfif";
 const main = () => {
   const [data, setdata] = useState([]);
   const [value, setvalue] = useState("");
+  const [price, setprice] = useState("");
+  const [desc, setdesc] = useState("");
   const handleCreate = (e) => {
     e.preventDefault();
     let obj = {
       id: new Date().getTime(),
       value,
       img: rasm,
-      price: 50000,
-      desc: "A book description is a brief overview of the plot, main characters, and themes of the story. It's an important tool that helps in book promotion and sales. Many times, book descriptions also include information about the author. This helps to build credibility and establish a connection with the reader.z",
+      price,
+      desc,
     };
     setdata((p) => [...p, obj]);
     setvalue("");
+    setprice;
+    setdesc;
   };
   console.log(data);
   return (
@@ -29,6 +33,22 @@ const main = () => {
           className="b bg-slate-100"
           value={value}
           onChange={(e) => setvalue(e.target.value)}
+          type="text"
+        />
+        <label htmlFor="">kitob narxi</label>
+        <input
+          required
+          className="b bg-slate-100"
+          value={price}
+          onChange={(e) => setprice(e.target.value)}
+          type="text"
+        />
+        <label htmlFor="">kitob izohi</label>
+        <input
+          required
+          className="b bg-slate-100"
+          value={desc}
+          onChange={(e) => setdesc(e.target.value)}
           type="text"
         />
         <button className="btn text-red-900">Creat</button>
